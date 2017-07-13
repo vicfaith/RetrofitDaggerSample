37b070bc3e957dc9d0663d94aed8e0599616a8cb
dkang-wow <dkang@woolworths.com.au>
2017-07-13 21:04:25 +1000

    jacoco

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ "$BUDDYBUILD_BRANCH" = "develop" ]]; then
-  # generate code coverage
-  ./gradlew clean jacocoTestReport
+# generate code coverage
+./gradlew clean jacocoTestReport
 
-  # upload jacoco report to codecov
-  bash <(curl -s https://codecov.io/bash) 
-fi
+# upload jacoco report to codecov
+bash <(curl -s https://codecov.io/bash)
